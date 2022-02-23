4e0489ebfdd8f433c1c267327cc7cd6020418dbf
卷毛 <47733889+zoengzihaam@users.noreply.github.com>
2022-02-23 15:40:57 +0800

    Modify default IP

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.5/g' package/base-files/files/bin/config_generate
 
 # Modify Host Name
 sed -i '/uci commit system/i uci set system.@system[0].hostname=CrazyWrt'  package/lean/default-settings/files/zzz-default-settings
